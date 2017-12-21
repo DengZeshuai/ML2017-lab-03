864bda21e293a0136638f340e22860fc9a90175b
DengZeshuai <1321730442@qq.com>
2017-12-21 15:07:47 +0800

    实验的py文件

--- ensemble.py
+++ ensemble.py
@@ -1,4 +1,5 @@
 import pickle
+import numpy as np
 
 class AdaBoostClassifier:
     '''A simple AdaBoost Classifier.'''
@@ -10,23 +11,54 @@ class AdaBoostClassifier:
             weak_classifier: The class of weak classifier, which is recommend to be sklearn.tree.DecisionTreeClassifier.
             n_weakers_limit: The maximum number of weak classifier the model can use.
         '''
-        pass
+        self.weak_classifier = weak_classifier
+        self.n_weakers_limit = n_weakers_limit
+        
 
     def is_good_enough(self):
         '''Optional'''
         pass
 
-    def fit(self,X,y):
+    def fit(self,X,y,max_depth=1):
         '''Build a boosted classifier from the training set (X, y).
 
         Returns:
             X: An ndarray indicating the samples to be trained, which shape should be (n_samples,n_features).
             y: An ndarray indicating the ground-truth labels correspond to X, which shape should be (n_samples,1).
         '''
-        pass
+        self.weights_of_samples = np.ones((X.shape[0],1),dtype=float) / X.shape[0]
+        self.prediction_of_classifiers = []
+        self.alphas_of_classifiers = []
+        self.classifiers = []
+        
+        print("Train start!")
+        for m in range(self.n_weakers_limit):
+            weights_of_samples = self.weights_of_samples.reshape(X.shape[0])
+            
+            new_weak_classifier = self.weak_classifier(max_depth=max_depth, splitter="random", random_state=1)
+            new_weak_classifier.fit(X,y, sample_weight=weights_of_samples)
+            self.classifiers.append(new_weak_classifier)
+            
+            prediction = new_weak_classifier.predict(X)
+            prediction = np.array(prediction).reshape((X.shape[0],1))
+            
+            #compute error
+            error = np.zeros(prediction.shape)
+            error[prediction != y] = 1
+            error_m = (self.weights_of_samples*error).sum()
+            if error_m > 0.5:
+                print("Train error: one of error rate less than 0.5")
+                break
+            
+            #compute weight of classifier and samples
+            alpha_m = 0.5*np.log((1.0-error_m)/ error_m)
+            # print(alpha_m)
+            self.alphas_of_classifiers.append(alpha_m)
+            Z_m = (self.weights_of_samples*np.exp(-alpha_m*y*prediction)).sum()
+            self.weights_of_samples =  self.weights_of_samples/Z_m * np.exp(-alpha_m*y*prediction)
+        
 
-
-    def predict_scores(self, X):
+    def predict_scores(self, X,y):
         '''Calculate the weighted sum score of the whole base classifiers for given samples.
 
         Args:
@@ -35,7 +67,12 @@ class AdaBoostClassifier:
         Returns:
             An one-dimension ndarray indicating the scores of differnt samples, which shape should be (n_samples,1).
         '''
-        pass
+        prediction_of_Adaboost = self.predict(X)
+        prediction_score_of_Adaboost = prediction_of_Adaboost*y
+        prediction_score_of_Adaboost[prediction_score_of_Adaboost==-1] == 0
+        
+        accuracy = prediction_score_of_Adaboost.sum()/prediction_score_of_Adaboost.shape[0]
+        return accuracy
 
     def predict(self, X, threshold=0):
         '''Predict the catagories for geven samples.
@@ -47,7 +84,15 @@ class AdaBoostClassifier:
         Returns:
             An ndarray consists of predicted labels, which shape should be (n_samples,1).
         '''
-        pass
+        prediction_of_Adaboost = np.zeros((X.shape[0],1))
+        for i in range(self.n_weakers_limit):
+                classifier_predict = np.array(self.classifiers[i].predict(X)).reshape((X.shape[0],1))
+                prediction_of_Adaboost += self.alphas_of_classifiers[i]*classifier_predict
+        
+        # classification
+        prediction_of_Adaboost[prediction_of_Adaboost>threshold] = 1
+        prediction_of_Adaboost[prediction_of_Adaboost<=threshold] = -1
+        return prediction_of_Adaboost
 
     @staticmethod
     def save(model, filename):
--- train.py
+++ train.py
@@ -1,6 +1,74 @@
+from PIL import Image
+import os
+from feature import NPDFeature
+import numpy  as np
+import pickle
+from ensemble import AdaBoostClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import model_selection
+
+def preprocess(face_path, nonface_path):
+    image_list = []
+    for image in os.listdir(face_path):
+        if image.endswith('.jpg'):
+            image_gray_scaled = Image.open(os.path.join(face_path, image)).convert('L').resize((24,24),Image.ANTIALIAS)
+            image_list.append(np.array(image_gray_scaled))
+    
+    for image in os.listdir(nonface_path):
+        if image.endswith('.jpg'):
+            image_gray_scaled = Image.open(os.path.join(nonface_path, image)).convert('L').resize((24,24),Image.ANTIALIAS)
+            image_list.append(np.array(image_gray_scaled))
+
+    return image_list, labels
+
+def  extract_features(image_list):
+    for index, image in enumerate(image_list):
+        image_list[index] = NPDFeature(image).extract()
+    return image_list
+
+
+def  main(cache_file, max_depth=2, n_weakers_limit=10, epoch=10):
+    # dump data to cache
+    if not open(cache_file, "rb"):
+        #preprocess the image to gray image and resize to 24*24
+        image_list = preprocess('datasets/original/face', 'datasets/original/nonface')
+        print("dump data to cache!")
+        data = extract_features(image_list)
+        AdaBoostClassifier.save(data ,'data_cache')
+        
+    # load data from cache
+    samples = AdaBoostClassifier.load(cache_file)
+    
+    labels = np.ones((len(samples),1))
+    for i in range(int(len(samples)/2),len(samples)):
+        labels[i] = -1
+
+    # preprocess data to ndarray
+    X, y = np.array(samples), np.array(labels).reshape(len(samples),1)
+    X_train, X_test, y_train, y_test = model_selection.train_test_split(X,y,test_size=0.4,random_state=33)
+    
+    model = AdaBoostClassifier(DecisionTreeClassifier, n_weakers_limit)
+    model.fit(X_train,y_train, max_depth)
+    
+    predict_score_of_Adaboost = model.predict_scores(X_train, y_train)
+    print("Train accuracy of Adaboost", predict_score_of_Adaboost)
+    predict_score_of_Adaboost = model.predict_scores(X_test, y_test)
+    print("Test accuracy of Adaboost", predict_score_of_Adaboost)
+    
+    print("Finish!")
 
 
 if __name__ == "__main__":
     # write your code here
-    pass
+    hyperparameter ={
+        'max_depth': 1,
+        "n_weakers_limit":10,
+        "cache_file":"data_cache"
+    }
+    main(**hyperparameter)
+
+
+
+
+
 
